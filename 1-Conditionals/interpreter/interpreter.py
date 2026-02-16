add168b7fc43fc77f2796333e7eccf3808570433
xylia(thanh) <xylia.nguyen980@education.nsw.gov.au>
2026-02-16 23:17:09 +0000

    2nd presuming (choke)

--- 1-Conditionals/interpreter/interpreter.py
+++ 1-Conditionals/interpreter/interpreter.py
@@ -1,11 +1,18 @@
 #promt user for expression
-
 expression = input('Expression: ')
 #split expression to 3 parts
 x, y, z = expression.split(" ")
 #float
 x = float(x)
 z = float(z)
+#calculate the result
+if y == '+':
+    result = x + z
+    print(result)
+elif y == '-':
+    result = x - z
+    print(result)
+
     
 
     
