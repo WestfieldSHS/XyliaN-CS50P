c630e826afd3ff8430ea4b0b87e9e163af56f725
xylia(thanh) <xylia.nguyen980@education.nsw.gov.au>
2026-02-16 23:11:52 +0000

    presume

---
+++ 1-Conditionals/interpreter/interpreter.py
@@ -0,0 +1,13 @@
+#promt user for expression
+
+expression = input('Expression: ')
+#split expression to 3 parts
+x, y, z = expression.split(" ")
+#float
+x = float(x)
+z = float(z)
+    
+
+    
+
+
